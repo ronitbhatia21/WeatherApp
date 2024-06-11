2c9b2297e8250989da2efcb7a97c9bdf0c38c628
ronithex <ronit.hexbis@gmail.com>
2024-06-11 13:08:16 +0530

    Final Commit

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -87,7 +87,6 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<WeatherApp>, t: Throwable) {
-                TODO("Not yet implemented")
             }
 
         })
